192fab7932fd5cb0c40f715de13ee8ee949ea550
Samir Ahmed <samirahmed2013@gmail.com>
2013-01-18 19:18:40 -0500

    Fixed js activation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@ $( function(){
 		if ($('.location-days').length > 0 )
 		{
 			var today = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'][new Date().getDay()];
-			$('.location-days a[href$="'+today+'"]').parent().addClass("active");
+			$('.location-days a[data$="'+today+'"]').parent().addClass("active");
 			$("#location-info").load('/day/'+today+' #info');
 			$('.location-days a').on('click',function(){
 				$("#location-info").load('/day/'+$(this).attr('data')+' #info');
